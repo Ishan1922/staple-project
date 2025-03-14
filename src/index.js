41437cbfef24034f1fb5ebc679e6e73c8f056162
ishan <ishanvadalia1607@gmail.com>
2025-03-14 14:38:17 +0530

    react render check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TestComponent = () => <h1>Hello, React is working!</h1>;
+
+ReactDOM.render(<TestComponent />, document.getElementById('root'));
+
