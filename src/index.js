5760166aba3df79a9b2afcf23a23f2ac2f31930b
ishan <ishanvadalia1607@gmail.com>
2025-03-14 15:00:05 +0530

    reverting changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const TestComponent = () => <h1>Hello, React is working!</h1>;
-
-ReactDOM.render(<TestComponent />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
